2e841aa63d1f4044058ae39d5806157d4937f91a
L-enze <143789408+L-enze@users.noreply.github.com>
2024-04-22 13:36:58 +0200

    nav expreriment

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-/*
-  Slidemenu
-*/
 (function() {
 	const $body = document.body
-	, $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
-
+	 $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
 	if ( typeof $menu_trigger !== 'undefined' ) {
 		$menu_trigger.addEventListener('click', function() {
 			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
 		});
 	}
-
 }).call(this);
