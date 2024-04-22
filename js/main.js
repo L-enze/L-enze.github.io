5b32f81a1958827370760ea2e8bfca574773b6fe
L-enze <143789408+L-enze@users.noreply.github.com>
2024-04-22 13:42:17 +0200

    defentieve navbar

--- js/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	const $body = document.body
-	 $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
-	if ( typeof $menu_trigger !== 'undefined' ) {
-		$menu_trigger.addEventListener('click', function() {
-			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
-		});
-	}
-}).call(this);
